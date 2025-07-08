c27a6cec2c1e66b47f592e41985702ad5d16a419
LENOVO <LENOVO@DESKTOP-IN5HT21>
2025-07-07 21:57:24 -0600

    Cors implementados

--- src/api/autorApiPosgreSql.js
+++ src/api/autorApiPosgreSql.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://microservicioautorlibro.somee.com/api/autor', // Asegúrate que esta URL sea accesible desde el frontend
+  baseURL: 'http://microservicioautorlibro.somee.com/api/autor', // Asegúrate que esta URL sea accesible desde el frontend
   headers: {
     'Content-Type': 'application/json',
   },
